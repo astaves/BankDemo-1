eca940db97b94ee3b0cbd4e99b8a920bcf7610ff
rskelton-rocket <rskelton@rocketsoftware.com>
2025-07-03 11:13:57 +0100

    Update MFE branding (#28)
    * Update branding of 'MFE' (Mainframe Express) to 'ES' (Enterprise Server).
    Modules: DFHZCSGM.bms BBANK90P.cbl ZBNKEXT1.cbl ZBNKEXT1.pli
    
    * Update 'WS-INF-DATA07' to not include letter mapping for phone number.
    * Remove duplicate phone number in 'Obtain more information' section.
    Modules: BBANK90P.cbl

--- sources/cobol/core/BBANK90P.cbl
+++ sources/cobol/core/BBANK90P.cbl
@@ -66,20 +66,15 @@
                VALUE '                                   '.
          05  WS-INF-DATA05.
            10  FILLER                              PIC X(40)
-               VALUE '     http://www.microfocus.com          '.
+               VALUE '    https://www.rocketsoftware.com      '.
            10  FILLER                              PIC X(35)
                VALUE '                                   '.
          05  WS-INF-DATA07.
            10  FILLER                              PIC X(40)
-               VALUE 'or call our office at 1-800-VS-COBOL    '.
+               VALUE 'or call our office at 1-855-577-4323    '.
            10  FILLER                              PIC X(35)
                VALUE '                                   '.
-         05  WS-INF-DATA08.
-           10  FILLER                              PIC X(40)
-               VALUE '                     (1-800-872-6265)   '.
-           10  FILLER                              PIC X(35)
-               VALUE '                                   '.
-         05  WS-INF-DATA10.
+         05  WS-INF-DATA09.
            10  FILLER                              PIC X(40)
                VALUE 'Thank you for your interest.            '.
            10  FILLER                              PIC X(35)
@@ -252,8 +247,7 @@
               MOVE WS-INF-DATA03 TO HELP01O-L03
               MOVE WS-INF-DATA05 TO HELP01O-L05
               MOVE WS-INF-DATA07 TO HELP01O-L07
-              MOVE WS-INF-DATA08 TO HELP01O-L08
-              MOVE WS-INF-DATA10 TO HELP01O-L10
+              MOVE WS-INF-DATA09 TO HELP01O-L09
            END-IF.
            MOVE HELP01O-DATA TO BANK-SCREEN90-DATA.
        POPULATE-SCR90-DATA-EXIT.
